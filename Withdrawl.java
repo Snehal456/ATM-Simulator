b6be4cc8074c0f12e10b7b966466f06b531ed97b
Snehal Pingat <155762168+Snehal456@users.noreply.github.com>
2025-06-12 00:29:15 +0530

    Update Withdrawl.java

--- Withdrawl.java
+++ Withdrawl.java
@@ -1,112 +1,83 @@
-
-package ASimulatorSystem;
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-import java.util.Date;
 import java.sql.*;
 
-public class Withdrawl extends JFrame implements ActionListener{
-    
-    JTextField t1,t2;
-    JButton b1,b2,b3;
-    JLabel l1,l2,l3,l4;
-    String pin;
-    Withdrawl(String pin){
-        this.pin = pin;
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/atm.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel l3 = new JLabel(i3);
-        l3.setBounds(0, 0, 960, 1080);
-        add(l3);
-        
-        l1 = new JLabel("MAXIMUM WITHDRAWAL IS RS.10,000");
-        l1.setForeground(Color.WHITE);
-        l1.setFont(new Font("System", Font.BOLD, 16));
-        
-        l2 = new JLabel("PLEASE ENTER YOUR AMOUNT");
-        l2.setForeground(Color.WHITE);
-        l2.setFont(new Font("System", Font.BOLD, 16));
-        
-        t1 = new JTextField();
-        t1.setFont(new Font("Raleway", Font.BOLD, 25));
-        
-        b1 = new JButton("WITHDRAW");
-        b2 = new JButton("BACK");
-        
+public class Withdrawl extends JFrame implements ActionListener {
+
+    JTextField amountField;
+    JButton withdrawBtn, backBtn;
+    String cardno;
+
+    Withdrawl(String cardno) {
+        this.cardno = cardno;
+
+        setTitle("Withdraw");
         setLayout(null);
-        
-        l1.setBounds(190,350,400,20);
-        l3.add(l1);
-        
-        l2.setBounds(190,400,400,20);
-        l3.add(l2);
-        
-        t1.setBounds(190,450,330,30);
-        l3.add(t1);
-        
-        b1.setBounds(390,588,150,35);
-        l3.add(b1);
-        
-        b2.setBounds(390,633,150,35);
-        l3.add(b2);
-        
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-        
-        setSize(960,1080);
-        setLocation(500,0);
-        setUndecorated(true);
+
+        JLabel label = new JLabel("Enter Amount to Withdraw:");
+        label.setBounds(50, 50, 200, 30);
+        add(label);
+
+        amountField = new JTextField();
+        amountField.setBounds(250, 50, 150, 30);
+        add(amountField);
+
+        withdrawBtn = new JButton("Withdraw");
+        withdrawBtn.setBounds(100, 120, 100, 30);
+        withdrawBtn.addActionListener(this);
+        add(withdrawBtn);
+
+        backBtn = new JButton("Back");
+        backBtn.setBounds(220, 120, 100, 30);
+        backBtn.addActionListener(this);
+        add(backBtn);
+
+        setSize(450, 250);
+        setLocation(600, 300);
         setVisible(true);
-    }
-    
-    
-    public void actionPerformed(ActionEvent ae){
-        try{        
-            String amount = t1.getText();
-            Date date = new Date();
-            if(ae.getSource()==b1){
-                if(t1.getText().equals("")){
-                    JOptionPane.showMessageDialog(null, "Please enter the Amount to you want to Withdraw");
-                }else{
-                    Conn c1 = new Conn();
-                    
-                    ResultSet rs = c1.s.executeQuery("select * from bank where pin = '"+pin+"'");
-                    int balance = 0;
-                    while(rs.next()){
-                       if(rs.getString("mode").equals("Deposit")){
-                           balance += Integer.parseInt(rs.getString("amount"));
-                       }else{
-                           balance -= Integer.parseInt(rs.getString("amount"));
-                       }
-                    }
-                    if(balance < Integer.parseInt(amount)){
-                        JOptionPane.showMessageDialog(null, "Insuffient Balance");
-                        return;
-                    }
-                    
-                    c1.s.executeUpdate("insert into bank values('"+pin+"', '"+date+"', 'Withdrawl', '"+amount+"')");
-                    JOptionPane.showMessageDialog(null, "Rs. "+amount+" Debited Successfully");
-                    
-                    setVisible(false);
-                    new Transactions(pin).setVisible(true);
-                }
-            }else if(ae.getSource()==b2){
-                setVisible(false);
-                new Transactions(pin).setVisible(true);
-            }
-        }catch(Exception e){
-                e.printStackTrace();
-                System.out.println("error: "+e);
-        }
-            
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
-    
-    
-    public static void main(String[] args){
-        new Withdrawl("").setVisible(true);
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == withdrawBtn) {
+            String amount = amountField.getText();
+            if (amount.equals("")) {
+                JOptionPane.showMessageDialog(null, "Enter amount");
+            } else {
+                try {
+                    Conn c = new Conn();
+
+                    // Calculate current balance
+                    ResultSet rs = c.s.executeQuery("SELECT type, amount FROM bank WHERE card_no = '" + cardno + "'");
+                    double balance = 0;
+                    while (rs.next()) {
+                        if (rs.getString("type").equals("Deposit")) {
+                            balance += rs.getDouble("amount");
+                        } else {
+                            balance -= rs.getDouble("amount");
+                        }
+                    }
+
+                    double withdrawAmount = Double.parseDouble(amount);
+                    if (withdrawAmount > balance) {
+                        JOptionPane.showMessageDialog(null, "Insufficient Balance");
+                    } else {
+                        String q = "INSERT INTO bank (card_no, type, amount) VALUES ('" + cardno + "', 'Withdraw', "
+                                + withdrawAmount + ")";
+                        c.s.executeUpdate(q);
+                        JOptionPane.showMessageDialog(null, "Rs. " + amount + " Withdrawn Successfully");
+                        setVisible(false);
+                        new Transactions(cardno).setVisible(true);
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        } else if (ae.getSource() == backBtn) {
+            setVisible(false);
+            new Transactions(cardno).setVisible(true);
+        }
     }
 }
