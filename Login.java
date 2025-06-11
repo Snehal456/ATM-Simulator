188c48eee7074b4200d8b1b7694d14a8bc9269fc
Snehal Pingat <155762168+Snehal456@users.noreply.github.com>
2025-06-12 00:24:51 +0530

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,120 +1,89 @@
-package ASimulatorSystem;
-
+// his will show the GUI login screen and call the Conn class to validate login credentials from the database.
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 import java.sql.*;
 
-public class Login extends JFrame implements ActionListener{
-    JLabel l1,l2,l3;
-    JTextField tf1;
-    JPasswordField pf2;
-    JButton b1,b2,b3;
-  
-    Login(){
-        setTitle("AUTOMATED TELLER MACHINE");
-        
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/logo.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel l11 = new JLabel(i3);
-        l11.setBounds(70, 10, 100, 100);
-        add(l11);
-        
-        l1 = new JLabel("WELCOME TO ATM");
-        l1.setFont(new Font("Osward", Font.BOLD, 38));
-        l1.setBounds(200,40,450,40);
-        add(l1);
-        
-        l2 = new JLabel("Card No:");
-        l2.setFont(new Font("Raleway", Font.BOLD, 28));
-        l2.setBounds(125,150,375,30);
-        add(l2);
-        
-        tf1 = new JTextField(15);
-        tf1.setBounds(300,150,230,30);
-        tf1.setFont(new Font("Arial", Font.BOLD, 14));
-        add(tf1);
-        
-        l3 = new JLabel("PIN:");
-        l3.setFont(new Font("Raleway", Font.BOLD, 28));
-        l3.setBounds(125,220,375,30);
-        add(l3);
-        
-        pf2 = new JPasswordField(15);
-        pf2.setFont(new Font("Arial", Font.BOLD, 14));
-        pf2.setBounds(300,220,230,30);
-        add(pf2);
-                
-        b1 = new JButton("SIGN IN");
-        b1.setBackground(Color.BLACK);
-        b1.setForeground(Color.WHITE);
-        
-        b2 = new JButton("CLEAR");
-        b2.setBackground(Color.BLACK);
-        b2.setForeground(Color.WHITE);
-        
-        b3 = new JButton("SIGN UP");
-        b3.setBackground(Color.BLACK);
-        b3.setForeground(Color.WHITE);
-        
-        setLayout(null);
-        
-        b1.setFont(new Font("Arial", Font.BOLD, 14));
-        b1.setBounds(300,300,100,30);
-        add(b1);
-        
-        b2.setFont(new Font("Arial", Font.BOLD, 14));
-        b2.setBounds(430,300,100,30);
-        add(b2);
-        
-        b3.setFont(new Font("Arial", Font.BOLD, 14));
-        b3.setBounds(300,350,230,30);
-        add(b3);
-        
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-        b3.addActionListener(this);
-        
-        getContentPane().setBackground(Color.WHITE);
-        
-        setSize(800,480);
-        setLocation(550,200);
-        setVisible(true);
-        
-    }
-    public void actionPerformed(ActionEvent ae){
-        try{        
-            if(ae.getSource()==b1){
-                Conn c1 = new Conn();
-                String cardno  = tf1.getText();
-                String pin  = pf2.getText();
-                String q  = "select * from login where cardno = '"+cardno+"' and pin = '"+pin+"'";
+public class Login extends JFrame implements ActionListener {
 
+    JLabel l1, l2;
+    JTextField tf1;
+    JPasswordField pf1;
+    JButton b1, b2, b3; // Added Register button
+
+    Login() {
+        super("ATM Login");
+
+        l1 = new JLabel("Card No:");
+        l1.setBounds(50, 40, 100, 30);
+        add(l1);
+
+        l2 = new JLabel("PIN:");
+        l2.setBounds(50, 90, 100, 30);
+        add(l2);
+
+        tf1 = new JTextField();
+        tf1.setBounds(150, 40, 200, 30);
+        add(tf1);
+
+        pf1 = new JPasswordField();
+        pf1.setBounds(150, 90, 200, 30);
+        add(pf1);
+
+        b1 = new JButton("Sign In");
+        b1.setBounds(50, 150, 90, 30);
+        b1.addActionListener(this);
+        add(b1);
+
+        b2 = new JButton("Clear");
+        b2.setBounds(150, 150, 90, 30);
+        b2.addActionListener(this);
+        add(b2);
+
+        b3 = new JButton("Register");
+        b3.setBounds(250, 150, 100, 30);
+        b3.addActionListener(this);
+        add(b3);
+
+        setLayout(null);
+        setSize(420, 280);
+        setLocation(600, 300);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == b1) { // Sign In
+            try {
+                String cardno = tf1.getText();
+                String pin = new String(pf1.getPassword());
+
+                Conn c1 = new Conn();
+                String q = "SELECT * FROM login WHERE card_no = '" + cardno + "' AND pin = '" + pin + "'";
                 ResultSet rs = c1.s.executeQuery(q);
-                if(rs.next()){
+
+                if (rs.next()) {
                     setVisible(false);
-                    new Transactions(pin).setVisible(true);
-                }else{
-                    JOptionPane.showMessageDialog(null, "Incorrect Card Number or PIN");
+                    new Transactions(cardno).setVisible(true);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Incorrect Card No or PIN");
                 }
-            }else if(ae.getSource()==b2){
-                tf1.setText("");
-                pf2.setText("");
-            }else if(ae.getSource()==b3){
-                setVisible(false);
-                new Signup().setVisible(true);
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        }catch(Exception e){
-            e.printStackTrace();
+
+        } else if (ae.getSource() == b2) { // Clear
+            tf1.setText("");
+            pf1.setText("");
+
+        } else if (ae.getSource() == b3) { // Register
+            setVisible(false);
+            new Signup().setVisible(true);
         }
     }
-    public static void main(String[] args){
-        new Login().setVisible(true);
+
+    public static void main(String[] args) {
+        new Login();
     }
-
-    
 }
-
-
-
