9618851cdb5593c861972fe386edaf013c92054a
Snehal Pingat <155762168+Snehal456@users.noreply.github.com>
2025-06-12 00:24:14 +0530

    Update Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -1,84 +1,65 @@
-
-package ASimulatorSystem;
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-import java.util.*;
+import java.sql.*;
 
-public class Deposit extends JFrame implements ActionListener{
-    
-    JTextField t1,t2;
-    JButton b1,b2,b3;
-    JLabel l1,l2,l3;
-    String pin;
-    Deposit(String pin){
-        this.pin = pin;
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/atm.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel l3 = new JLabel(i3);
-        l3.setBounds(0, 0, 960, 1080);
-        add(l3);
-        
-        l1 = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
-        l1.setForeground(Color.WHITE);
-        l1.setFont(new Font("System", Font.BOLD, 16));
-        
-        t1 = new JTextField();
-        t1.setFont(new Font("Raleway", Font.BOLD, 22));
-        
-        b1 = new JButton("DEPOSIT");
-        b2 = new JButton("BACK");
-        
+public class Deposit extends JFrame implements ActionListener {
+
+    JTextField amountField;
+    JButton depositBtn, backBtn;
+    String cardno;
+
+    Deposit(String cardno) {
+        this.cardno = cardno;
+
+        setTitle("Deposit");
         setLayout(null);
-        
-        l1.setBounds(190,350,400,35);
-        l3.add(l1);
-        
-        t1.setBounds(190,420,320,25);
-        l3.add(t1);
-        
-        b1.setBounds(390,588,150,35);
-        l3.add(b1);
-        
-        b2.setBounds(390,633,150,35);
-        l3.add(b2);
-        
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-        
-        setSize(960,1080);
-        setUndecorated(true);
-        setLocation(500,0);
+
+        JLabel label = new JLabel("Enter Amount to Deposit:");
+        label.setBounds(50, 50, 200, 30);
+        add(label);
+
+        amountField = new JTextField();
+        amountField.setBounds(250, 50, 150, 30);
+        add(amountField);
+
+        depositBtn = new JButton("Deposit");
+        depositBtn.setBounds(100, 120, 100, 30);
+        depositBtn.addActionListener(this);
+        add(depositBtn);
+
+        backBtn = new JButton("Back");
+        backBtn.setBounds(220, 120, 100, 30);
+        backBtn.addActionListener(this);
+        add(backBtn);
+
+        setSize(450, 250);
+        setLocation(600, 300);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    public void actionPerformed(ActionEvent ae){
-        try{        
-            String amount = t1.getText();
-            Date date = new Date();
-            if(ae.getSource()==b1){
-                if(t1.getText().equals("")){
-                    JOptionPane.showMessageDialog(null, "Please enter the Amount to you want to Deposit");
-                }else{
-                    Conn c1 = new Conn();
-                    c1.s.executeUpdate("insert into bank values('"+pin+"', '"+date+"', 'Deposit', '"+amount+"')");
-                    JOptionPane.showMessageDialog(null, "Rs. "+amount+" Deposited Successfully");
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == depositBtn) {
+            String amount = amountField.getText();
+            if (amount.equals("")) {
+                JOptionPane.showMessageDialog(null, "Enter amount");
+            } else {
+                try {
+                    Conn c = new Conn();
+                    String q = "INSERT INTO bank (card_no, type, amount) VALUES ('" + cardno + "', 'Deposit', " + amount
+                            + ")";
+                    c.s.executeUpdate(q);
+                    JOptionPane.showMessageDialog(null, "Rs. " + amount + " Deposited Successfully");
                     setVisible(false);
-                    new Transactions(pin).setVisible(true);
+                    new Transactions(cardno).setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }else if(ae.getSource()==b2){
-                setVisible(false);
-                new Transactions(pin).setVisible(true);
             }
-        }catch(Exception e){
-            e.printStackTrace();
+        } else if (ae.getSource() == backBtn) {
+            setVisible(false);
+            new Transactions(cardno).setVisible(true);
         }
-            
-    }
-    
-    public static void main(String[] args){
-        new Deposit("").setVisible(true);
     }
 }
