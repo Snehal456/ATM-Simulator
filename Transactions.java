e75b2d448eee3e39af054b87f926651f9eb9adad
Snehal Pingat <155762168+Snehal456@users.noreply.github.com>
2025-06-12 00:27:59 +0530

    Update Transactions.java

--- Transactions.java
+++ Transactions.java
@@ -1,106 +1,79 @@
-package ASimulatorSystem;
-
+// this is the main dashboard after login where users can choose:
+// Deposit
+// Withdraw
+// Balance Enquiry
+// Exit
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
-import java.sql.*;
 
-public class Transactions extends JFrame implements ActionListener{
+public class Transactions extends JFrame implements ActionListener {
 
-    JLabel l1;
-    JButton b1,b2,b3,b4,b5,b6,b7;
-    String pin;
-    Transactions(String pin){
-        this.pin = pin;
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("ASimulatorSystem/icons/atm.jpg"));
-        Image i2 = i1.getImage().getScaledInstance(1000, 1180, Image.SCALE_DEFAULT);
-        ImageIcon i3 = new ImageIcon(i2);
-        JLabel l2 = new JLabel(i3);
-        l2.setBounds(0, 0, 960, 1080);
-        add(l2);
-        
-        l1 = new JLabel("Please Select Your Transaction");
-        l1.setForeground(Color.WHITE);
-        l1.setFont(new Font("System", Font.BOLD, 16));
-        
-       
-        b1 = new JButton("DEPOSIT");
-        b2 = new JButton("CASH WITHDRAWL");
-        b3 = new JButton("FAST CASH");
-        b4 = new JButton("MINI STATEMENT");
-        b5 = new JButton("PIN CHANGE");
-        b6 = new JButton("BALANCE ENQUIRY");
-        b7 = new JButton("EXIT");
-        
+    JButton depositBtn, withdrawBtn, balanceBtn, miniStmtBtn, exitBtn;
+    String cardno;
+
+    Transactions(String cardno) {
+        this.cardno = cardno;
+
+        setTitle("ATM Transactions");
         setLayout(null);
-        
-        l1.setBounds(235,400,700,35);
-        l2.add(l1);
-        
-        b1.setBounds(170,499,150,35);
-        l2.add(b1);
-        
-        b2.setBounds(390,499,150,35);
-        l2.add(b2);
-        
-        b3.setBounds(170,543,150,35);
-        l2.add(b3);
-        
-        b4.setBounds(390,543,150,35);
-        l2.add(b4);
-        
-        b5.setBounds(170,588,150,35);
-        l2.add(b5);
-        
-        b6.setBounds(390,588,150,35);
-        l2.add(b6);
-        
-        b7.setBounds(390,633,150,35);
-        l2.add(b7);
-        
-        
-        b1.addActionListener(this);
-        b2.addActionListener(this);
-        b3.addActionListener(this);
-        b4.addActionListener(this);
-        b5.addActionListener(this);
-        b6.addActionListener(this);
-        b7.addActionListener(this);
-        
-        
-        setSize(960,1080);
-        setLocation(500,0);
-        setUndecorated(true);
+
+        JLabel label = new JLabel("Select Transaction");
+        label.setFont(new Font("System", Font.BOLD, 20));
+        label.setBounds(100, 30, 300, 30);
+        add(label);
+
+        depositBtn = new JButton("Deposit");
+        depositBtn.setBounds(100, 80, 150, 30);
+        depositBtn.addActionListener(this);
+        add(depositBtn);
+
+        withdrawBtn = new JButton("Withdraw");
+        withdrawBtn.setBounds(100, 120, 150, 30);
+        withdrawBtn.addActionListener(this);
+        add(withdrawBtn);
+
+        balanceBtn = new JButton("Balance Enquiry");
+        balanceBtn.setBounds(100, 160, 150, 30);
+        balanceBtn.addActionListener(this);
+        add(balanceBtn);
+
+        miniStmtBtn = new JButton("Mini Statement");
+        miniStmtBtn.setBounds(100, 200, 150, 30);
+        miniStmtBtn.addActionListener(this);
+        add(miniStmtBtn);
+
+        exitBtn = new JButton("Exit");
+        exitBtn.setBounds(100, 240, 150, 30);
+        exitBtn.addActionListener(this);
+        add(exitBtn);
+
+        setSize(400, 350);
+        setLocation(600, 300);
         setVisible(true);
-        
-        
-        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    public void actionPerformed(ActionEvent ae){
-        if(ae.getSource()==b1){ 
+
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == depositBtn) {
             setVisible(false);
-            new Deposit(pin).setVisible(true);
-        }else if(ae.getSource()==b2){ 
+            new Deposit(cardno).setVisible(true);
+        } else if (ae.getSource() == withdrawBtn) {
             setVisible(false);
-            new Withdrawl(pin).setVisible(true);
-        }else if(ae.getSource()==b3){ 
+            new Withdrawl(cardno).setVisible(true);
+        } else if (ae.getSource() == balanceBtn) {
             setVisible(false);
-            new FastCash(pin).setVisible(true);
-        }else if(ae.getSource()==b4){ 
-            new MiniStatement(pin).setVisible(true);
-        }else if(ae.getSource()==b5){ 
+            new BalanceEnquiry(cardno).setVisible(true);
+        } else if (ae.getSource() == miniStmtBtn) {
             setVisible(false);
-            new Pin(pin).setVisible(true);
-        }else if(ae.getSource()==b6){ 
-            this.setVisible(false);
-            new BalanceEnquiry(pin).setVisible(true);
-        }else if(ae.getSource()==b7){ 
+            new MiniStatement(cardno).setVisible(true);
+        } else if (ae.getSource() == exitBtn) {
+            JOptionPane.showMessageDialog(null, "Thank you for using ATM!");
             System.exit(0);
         }
     }
-    
-    public static void main(String[] args){
-        new Transactions("").setVisible(true);
+
+    public static void main(String[] args) {
+        new Transactions("123456789");  // For testing purpose
     }
 }
